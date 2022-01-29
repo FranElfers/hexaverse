bb46619c552084358dd7f54f82eec50c706128ef
FranElfers <fransozzan@gmail.com>
2022-01-29 09:25:18 -0300

    usar server aws solo en produccion

--- utils/socket_service.ts
+++ utils/socket_service.ts
@@ -1,7 +1,9 @@
 import io from'socket.io-client'
 
-// const socket = io('http://18.228.9.168:3000/')
-const socket = io('http://localhost:3001/')
+const socket = io(process.env.NODE_ENV === 'production' 
+	? 'http://18.228.9.168:3000/'
+	: 'http://localhost:3001/'
+)
 
 socket.on('connect', () => {
 	console.log('connected', socket.id)	
